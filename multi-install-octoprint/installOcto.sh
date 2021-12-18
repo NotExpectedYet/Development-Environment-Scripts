f6218f9b75b708e49f1bbd23bdb56255bd7dc5be
James Mackay <online@notexpectedyet.com>
2021-12-18 07:40:10 +0000

    update to support later OP versions

--- multi-install-octoprint/installOcto.sh
+++ multi-install-octoprint/installOcto.sh
@@ -38,15 +38,23 @@ done
 echo "Which version of OctoPrint would you like to install?"
 octoVersion=""
 PS3='Please enter your choice: '
-options=("1.6.1" "1.6.0" "1.5.3" "1.5.2" "1.5.1" "1.5.0" "Quit")
+options=("1.7.1", "1.7.0", "1.6.1" "1.6.0" "1.5.3" "1.5.2" "1.5.1" "1.5.0" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
+         "1.7.1")
+            octoVersion=$opt
+            echo "You chose: $opt"
+            break;;
+         "1.7.0")
+            octoVersion=$opt
+            echo "You chose: $opt"
+            break;;
          "1.6.1")
             octoVersion=$opt
             echo "You chose: $opt"
             break;;
-     	"1.6.0")
+     	  "1.6.0")
             octoVersion=$opt
             echo "You chose: $opt"
             break;;
